d896e58aa12928868f9022546a4818868ea2d3e1
Joshua Whiting <inejwstine@gmail.com>
2018-12-22 14:55:11 -0700

    visual studio solution setup

---
+++ TriangleInfo/TriangleInfoTests/TriangleInfoUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleInfoTests
+{
+    [TestClass]
+    public class TriangleInfoUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
