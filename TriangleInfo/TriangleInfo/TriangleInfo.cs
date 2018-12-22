681771eaa50daa60603497236ea94f15389055e7
Joshua Whiting <inejwstine@gmail.com>
2018-12-22 15:23:35 -0700

    adds GetTriangleType method and validates numerical input

--- TriangleInfo/TriangleInfo/TriangleInfo.cs
+++ TriangleInfo/TriangleInfo/TriangleInfo.cs
@@ -8,6 +8,14 @@ namespace TriangleInfo
 {
     public static class TriangleInfo
     {
-
+        public static void GetTriangleType(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0)
+                throw new ArgumentException("All sides of the triangle must be positive in length.", "sideA");
+            if (sideB <= 0)
+                throw new ArgumentException("All sides of the triangle must be positive in length.", "sideB");
+            if (sideC <= 0)
+                throw new ArgumentException("All sides of the triangle must be positive in length.", "sideC");
+        }
     }
 }
--- TriangleInfo/TriangleInfoTests/TriangleInfoUnitTests.cs
+++ TriangleInfo/TriangleInfoTests/TriangleInfoUnitTests.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleInfo;
 
 namespace TriangleInfoTests
 {
     [TestClass]
     public class TriangleInfoUnitTests
     {
+        #region Valid Input Tests
         [TestMethod]
-        public void TestMethod1()
+        public void GetTriangleType_ThrowsExceptionOnNegativeSideA()
         {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(-1, 1, 1));
         }
+        [TestMethod]
+        public void GetTriangleType_ThrowsExceptionOnNegativeSideB()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(1, -1, 1));
+        }
+        [TestMethod]
+        public void GetTriangleType_ThrowsExceptionOnNegativeSideC()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(1, 1, -1));
+        }
+        [TestMethod]
+        public void GetTriangleType_ThrowsExceptionOnZeroSideA()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(0, 1, 1));
+        }
+        [TestMethod]
+        public void GetTriangleType_ThrowsExceptionOnZeroSideB()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(1, 0, 1));
+        }
+        [TestMethod]
+        public void GetTriangleType_ThrowsExceptionOnZeroSideC()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TriangleInfo.TriangleInfo.GetTriangleType(1, 1, 0));
+        }
+        #endregion
     }
 }
